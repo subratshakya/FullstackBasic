d77b8de2913eb8d823320568739c29d676f74992
subratshakya <subratshakya20@gmail.com>
2025-04-10 19:46:39 +0530

    added jokes haha

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,38 @@ app.get('/', (req,res) => {
     res.send('Server is ready');
 });
 
+//get a list of 5 jokes
+
+app.get('/jokes', (req,res) => {
+    const jokes=[
+        {
+            id: 1,
+            title: 'Joke no. 1',
+            content:'JOKE'
+        },
+        {
+            id: 5,
+            title: 'Joke no. 1',
+            content:'JOKE'
+        },
+        {
+            id: 4,
+            title: 'Joke no. 1',
+            content:'JOKE'
+        },
+        {
+            id: 3,
+            title: 'Joke no. 1',
+            content:'JOKE'
+        },
+        {
+            id: 2,
+            title: 'Joke no. 1',
+            content:'JOKE'
+        }
+    ];
+    res.send(jokes);
+})
 const port=process.env.PORT || 3000;
 
 app.listen(port, () => {

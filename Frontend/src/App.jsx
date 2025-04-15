c40a5f09d347aa78ea2b924724e725a58fde5145
subratshakya <subratshakya20@gmail.com>
2025-04-15 16:05:30 +0530

    proxy in vite config

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,11 +6,12 @@ import axios from 'axios'
 function App() {
   const [jokes, setJokes] = useState([])
   useEffect(()=>{
-    axios.get('http://localhost:3000/jokes')
+    axios.get('/api/jokes')
     .then((response)=>{
       setJokes(response.data)
     }
   )
+
   .catch((error)=>{
     console.log(error)
   })
@@ -21,14 +22,14 @@ function App() {
       <p>Jokes: {jokes.length}</p>
 
       {
-        jokes.map((joke,index)=>{
+        jokes.map((joke,index)=>(
           <div key={joke.id}>
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
 
             
           </div>
-        })
+        ))
       }
     </>
   )
